c1734510b81c4d970b6f21fb7e1bc027daaed48b
Camilo Griffa <camilogriffa@protonmail.com>
2022-04-23 03:27:30 -0300

    primer commit del repo

---
+++ lenguaje_python/algoritmos.py
@@ -0,0 +1,22 @@
+# programa para saber si el usuario es mayor de edad
+edad = int(input("Ingresa tu edad: "))
+if edad>=18: print("Sos mayor de edad")
+else: print("Sos menor de edad")
+
+# programa para saber si tu sueldo
+# es mayor al sueldo minimo
+sueldoMinimo = int(input("Ingresa el valor del sueldo minimo: "))
+tuSueldo = int(input("Ingresa el valor de tu sueldo: "))
+if tuSueldo>=sueldoMinimo: print("Cobras igual o por arriba del mínimo")
+else: print("Tu sueldo es menor al mínimo vital")
+
+# programa para imprimir CORRECTO o INCORRECTO
+valorAEvaluar = str(input("Ingrese valor S o N: "))
+valoresCorrectos = str(input("Ingrese valores correctos: "))
+# if valorAEvaluar=valoresCorrectos: print("CORRECTO")
+# else: print("INCORRECTO")
+
+# programa para saber si un número es par o impar
+numero = int(input("Ingrese el valor de su número para saber si es par: "))
+
+
